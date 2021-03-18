3a8c1799051370b140673edfef6f022e6c0350d6
Jamie Mac <macster110@gmail.com>
2021-03-18 08:16:29 +0100

    First commit of test project

---
+++ JPAM_test_project/src/main/java/testjfrog/TestJFrog.java
@@ -0,0 +1,18 @@
+package testjfrog;
+
+import org.jamdev.jdl4pam.SoundSpot.SoundSpotParams;
+import org.jamdev.jpamutils.spectrogram.Spectrogram;
+
+public class TestJFrog {
+	
+	public static void main( String[] args ) {
+		
+		//does it compile???
+		SoundSpotParams dlParams = new SoundSpotParams(); 
+		System.out.println("Hello: " +  dlParams.seglen); 
+		
+		Spectrogram specgram;
+	}
+
+
+}
