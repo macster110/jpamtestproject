c1a820cbbfc99b3c538ee88278a71a08ceb10c87
Jamie Mac <macster110@gmail.com>
2021-05-05 14:28:32 +0200

    Testing TensorFlow not found bug when compiling jar file.

--- JPAM_test_project/src/main/java/testjfrog/TestJFrog.java
+++ JPAM_test_project/src/main/java/testjfrog/TestJFrog.java
@@ -1,18 +1,39 @@
 package testjfrog;
 
-import org.jamdev.jdl4pam.SoundSpot.SoundSpotParams;
-import org.jamdev.jpamutils.spectrogram.Spectrogram;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import ai.djl.MalformedModelException;
+import ai.djl.Model;
+import ai.djl.engine.Engine;
+
+/**
+ * Test loading a Tensorflow classifier using the jdl4pam library. 
+ * @author Jamie Macaulay
+ *
+ */
 public class TestJFrog {
 	
 	public static void main( String[] args ) {
+		args = new String[1]; 
+		args[0] = "C:\\Users\\Jamie\\Desktop\\Right_whales_DG\\model_lenet_dropout_input_conv_all\\saved_model.pb"; 
+		String modelPath = args[0]; 
+		File file = new File(modelPath);
+		Path modelDir = Paths.get(file.getAbsoluteFile().getParent()); 
+		String modelName = file.getName(); 
+	
+		System.out.println("Loading model: " + modelName);
 		
-		//does it compile???
-		SoundSpotParams dlParams = new SoundSpotParams(); 
-		System.out.println("Hello: " +  dlParams.seglen); 
-		
-		Spectrogram specgram;
+		System.out.println(Engine.getAllEngines()); 
+
+		Model model = Model.newInstance(modelPath, "TensorFlow");
+		try {
+			model.load(modelDir, "saved_model.pb");
+			System.out.println("Model loaded successfully: " + modelName);
+		} catch (MalformedModelException | IOException e) {
+			e.printStackTrace();
+		}
 	}
-
-
 }
