f0fd793f0bc9caacdd4c8caf3b16ade477991130
Jamie Mac <macster110@gmail.com>
2021-05-31 20:29:16 +0200

    Updatedtest code for %1 error

--- JPAM_test_project/src/main/java/testdjl/TestDJL.java
+++ JPAM_test_project/src/main/java/testdjl/TestDJL.java
@@ -15,45 +15,49 @@ import ai.djl.engine.Engine;
  *
  */
 public class TestDJL {
-	
+
 	public static void main( String[] args ) {
-		args = new String[2]; 
-		args[0] = "C:\\Users\\Jamie\\Desktop\\Right_whales_DG\\model_lenet_dropout_input_conv_all\\saved_model.pb"; 
-		args[1] = "C:\\Users\\Jamie\\Desktop\\pytorchmodel\\BAT_JAMIE_4ms_256fft_8hop_-100_20_15_60_128_256_NOJIT_BAT_DATA_NAUG_V1_JIT.pk"; 
-		
+		if (args==null || args.length<2) {
+			args = new String[2];
+			args[0] = "C:\\Users\\au671271\\Google Drive\\PAMGuard_dev\\Deep_Learning\\Right_whales_DG\\model_lenet_dropout_input_conv_all\\saved_model.pb";
+			//		args[1] = "C:\\Users\\au671271\\Google Drive\\PAMGuard_dev\\tutorials\\deep_learning\\bat_DL_tutorial\\BAT_MULTI_JAMIE_5ms_256fft_10hop_MM_10_90_128_256_JIT_PITCHTIMECHANGE_AUG_V1.pk";
+			args[1]= "C:\\Users\\au671271\\Desktop\\bat_pytorch\\Copy of BAT_JAMIE_4ms_256fft_8hop_-100_20_15_60_128_256_NOJIT_BAT_DATA_NAUG_V1_JIT.pk";
+		}
 		String modelPath = args[0]; 
 		File file = new File(modelPath);
 		Path modelDir = Paths.get(file.getAbsoluteFile().getParent()); 
 		String modelName = file.getName(); 
-	
-		System.out.println("Loading model 1: " + modelName);
-		
+
 		System.out.println(Engine.getAllEngines()); 
 
+		System.out.println("Loading model 1: " + modelName);
+
+
 		Model model = Model.newInstance(modelPath, "TensorFlow");
-		try {
-			model.load(modelDir, modelName);
-			System.out.println("Model loaded successfully: " + modelName);
-		} catch (MalformedModelException | IOException e) {
-			e.printStackTrace();
-		}
-		
-		modelPath = args[0]; 
+
+				try {
+					model.load(modelDir, modelName);
+					System.out.println("Model loaded successfully: " + modelName);
+				} catch (MalformedModelException | IOException e) {
+					e.printStackTrace();
+				}
+
+		modelPath = args[1]; 
 		file = new File(modelPath);
 		modelDir = Paths.get(file.getAbsoluteFile().getParent()); 
 		modelName = file.getName(); 
-	
+
 		System.out.println("Loading model 2: " + modelName);
-		
+
 		System.out.println(Engine.getAllEngines()); 
 
-		model = Model.newInstance(modelPath, "PyTorch");
-		try {
-			model.load(modelDir, modelName);
-			System.out.println("Model loaded successfully: " + modelName);
-		} catch (MalformedModelException | IOException e) {
-			e.printStackTrace();
-		}
-		
+				model = Model.newInstance(modelPath, "PyTorch");
+				try {
+					model.load(modelDir, modelName);
+					System.out.println("Model loaded successfully: " + modelName);
+				} catch (MalformedModelException | IOException e) {
+					e.printStackTrace();
+				}
+
 	}
 }
